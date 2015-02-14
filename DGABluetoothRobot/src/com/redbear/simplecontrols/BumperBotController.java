391d7c2eaf70dc78fdafba5bb7a8c6fe32c5a440
dga148@utulsa.edu <dga148@utulsa.edu>
2015-02-13 18:59:20 -0600

    Added missing files

--- DGABluetoothRobot/src/com/redbear/simplecontrols/BumperBotController.java
+++ DGABluetoothRobot/src/com/redbear/simplecontrols/BumperBotController.java
@@ -23,24 +23,26 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
-import android.view.DragEvent;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class ControllerActivity extends Activity {
-	private final static String TAG = "DGABluetoothRobot.Controller";
+public class BumperBotController extends Activity {
+	private final static String TAG = BumperBotController.class.getSimpleName();
 
-	private Button leftButton;
-	private Button upButton;
-	private Button rightButton;
 
 	private BluetoothGattCharacteristic characteristicTx = null;
 	private RBLService mBluetoothLeService;
@@ -57,7 +59,7 @@ public class ControllerActivity extends Activity {
 	private static final long SCAN_PERIOD = 2000;
 
 	final private static char[] hexArray = { '0', '1', '2', '3', '4', '5', '6',
-		'7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+			'7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
 	private final ServiceConnection mServiceConnection = new ServiceConnection() {
 
@@ -86,7 +88,7 @@ public class ControllerActivity extends Activity {
 			if (RBLService.ACTION_GATT_DISCONNECTED.equals(action)) {
 				Toast.makeText(getApplicationContext(), "Disconnected",
 						Toast.LENGTH_SHORT).show();
-				disableButtons();
+				setButtonDisable();
 			} else if (RBLService.ACTION_GATT_SERVICES_DISCOVERED
 					.equals(action)) {
 				Toast.makeText(getApplicationContext(), "Connected",
@@ -95,84 +97,81 @@ public class ControllerActivity extends Activity {
 				getGattService(mBluetoothLeService.getSupportedGattService());
 			} else if (RBLService.ACTION_DATA_AVAILABLE.equals(action)) {
 				data = intent.getByteArrayExtra(RBLService.EXTRA_DATA);
-				Log.d("btData", String.valueOf(data));
+
+				readAnalogInValue(data);
 			} else if (RBLService.ACTION_GATT_RSSI.equals(action)) {
-				//displayData(intent.getStringExtra(RBLService.EXTRA_DATA));
+				displayData(intent.getStringExtra(RBLService.EXTRA_DATA));
 			}
 		}
 	};
-
+	int speed = 0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_controller);
-		leftButton = ((Button)findViewById(R.id.left_button));
-//		upButton = ((Button)findViewById(R.id.up_button));
-		rightButton = ((Button)findViewById(R.id.right_button));
-
-		//A normal OnClickListener will not do because for this app,
-		//the user will touch down and hold the button while they want the robot to move
-		//So use a touch listener which will listen for touchDown and touchUp
-		leftButton.setOnTouchListener(buttonTouchListener);
-//		upButton.setOnTouchListener(buttonTouchListener);
-		rightButton.setOnTouchListener(buttonTouchListener);
-
+		
 		if (!getPackageManager().hasSystemFeature(
 				PackageManager.FEATURE_BLUETOOTH_LE)) {
 			Toast.makeText(this, "Ble not supported", Toast.LENGTH_SHORT)
-			.show();
+					.show();
 			finish();
 		}
 
+		((Button)findViewById(R.id.left_button)).setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if(event.getAction() == MotionEvent.ACTION_DOWN) {
+					left();
+				}
+				else if(event.getAction() == MotionEvent.ACTION_UP) {
+					stop();
+				}
+				return false;
+			}
+		});
+		
+		
+		((Button)findViewById(R.id.right_button)).setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if(event.getAction() == MotionEvent.ACTION_DOWN) {
+					right();
+				}
+				else if(event.getAction() == MotionEvent.ACTION_UP) {
+					stop();
+				}
+				return false;
+			}
+		});
+		
+		((Button)findViewById(R.id.up_button)).setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if(event.getAction() == MotionEvent.ACTION_DOWN) {
+					forward();
+				}
+				else if(event.getAction() == MotionEvent.ACTION_UP) {
+					stop();
+				}
+				return false;
+			}
+		});
 		final BluetoothManager mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
 		mBluetoothAdapter = mBluetoothManager.getAdapter();
 		if (mBluetoothAdapter == null) {
 			Toast.makeText(this, "Ble not supported", Toast.LENGTH_SHORT)
-			.show();
+					.show();
 			finish();
 			return;
 		}
-//		((SeekBar)findViewById(R.id.seekBar)).setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-//			
-//			@Override
-//			public void onStopTrackingTouch(SeekBar seekBar) {
-//				int progress = seekBar.getProgress();
-//				characteristicTx.setValue(new byte[] {0x01,(byte)progress});
-//				mBluetoothLeService.writeCharacteristic(characteristicTx);
-//				Toast.makeText(getBaseContext(), "speed " + progress, Toast.LENGTH_SHORT).show();
-//			}
-//			
-//			@Override
-//			public void onStartTrackingTouch(SeekBar seekBar) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//			
-//			@Override
-//			public void onProgressChanged(SeekBar seekBar, int progress,
-//					boolean fromUser) {
-//				
-//			}
-//		});
-		//disableButtons();
 
-		Intent gattServiceIntent = new Intent(ControllerActivity.this,
+		Intent gattServiceIntent = new Intent(BumperBotController.this,
 				RBLService.class);
-
 		bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-
-		if (!mBluetoothAdapter.isEnabled()) {
-			Intent enableBtIntent = new Intent(
-					BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-		}
-
-		registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
 	}
 
 	@Override
@@ -182,7 +181,7 @@ public class ControllerActivity extends Activity {
 		getMenuInflater().inflate(R.menu.controller, menu);
 		return true;
 	}
-
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
@@ -198,44 +197,88 @@ public class ControllerActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
+	
 	@Override
-	protected void onStop() {
-		super.onStop();
+	protected void onResume() {
+		super.onResume();
 
-		flag = false;
-
-		unregisterReceiver(mGattUpdateReceiver);
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-
-		if (mServiceConnection != null)
-			unbindService(mServiceConnection);
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// User chose not to enable Bluetooth.
-		if (requestCode == REQUEST_ENABLE_BT
-				&& resultCode == Activity.RESULT_CANCELED) {
-			finish();
-			return;
+		if (!mBluetoothAdapter.isEnabled()) {
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 		}
 
-		super.onActivityResult(requestCode, resultCode, data);
+		registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+	}
+
+	private void displayData(String data) {
+//		if (data != null) {
+//			rssiValue.setText(data);
+//		}
+	}
+
+	private void readAnalogInValue(byte[] data) {
+//		for (int i = 0; i < data.length; i += 3) {
+//			if (data[i] == 0x0A) {
+//				if (data[i + 1] == 0x01)
+//					digitalInBtn.setChecked(false);
+//				else
+//					digitalInBtn.setChecked(true);
+//			} else if (data[i] == 0x0B) {
+//				int Value;
+//
+//				Value = ((data[i + 1] << 8) & 0x0000ff00)
+//						| (data[i + 2] & 0x000000ff);
+//
+//				AnalogInValue.setText(Value + "");
+//			}
+//		}
+	}
+
+	private void setButtonEnable() {
+		flag = true;
+		connState = true;
+//
+//		digitalOutBtn.setEnabled(flag);
+//		AnalogInBtn.setEnabled(flag);
+//		servoSeekBar.setEnabled(flag);
+//		PWMSeekBar.setEnabled(flag);
+//		connectBtn.setText("Disconnect");
+	}
+
+	private void setButtonDisable() {
+		flag = false;
+		connState = false;
+//
+//		digitalOutBtn.setEnabled(flag);
+//		AnalogInBtn.setEnabled(flag);
+//		servoSeekBar.setEnabled(flag);
+//		PWMSeekBar.setEnabled(flag);
+//		connectBtn.setText("Connect");
+	}
+
+	private void startReadRssi() {
+		new Thread() {
+			public void run() {
+
+				while (flag) {
+					mBluetoothLeService.readRssi();
+					try {
+						sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+		}.start();
 	}
 
 	private void getGattService(BluetoothGattService gattService) {
 		if (gattService == null)
 			return;
 
-		enableButtons();
-		//Kept track of signal strength,
-		//not really needed for this
-		//startReadRssi();
+		setButtonEnable();
+		startReadRssi();
 
 		characteristicTx = gattService
 				.getCharacteristic(RBLService.UUID_BLE_SHIELD_TX);
@@ -281,7 +324,7 @@ public class ControllerActivity extends Activity {
 						for(int i = 0; i < availableArduinos.size(); i++) 
 							arduinoNames[i] = availableArduinos.get(i).getName();
 						if(availableArduinos.size() > 0) {
-							AlertDialog.Builder builder = new AlertDialog.Builder(ControllerActivity.this);
+							AlertDialog.Builder builder = new AlertDialog.Builder(BumperBotController.this);
 							builder.setTitle("Choose a Device")
 							.setItems(arduinoNames, new DialogInterface.OnClickListener() {
 								public void onClick(DialogInterface dialog, int which) {
@@ -292,13 +335,14 @@ public class ControllerActivity extends Activity {
 							builder.create().show();
 						}
 						else {
-							Toast.makeText(ControllerActivity.this, "No devices found", Toast.LENGTH_SHORT).show();
+							Toast.makeText(BumperBotController.this, "No devices found", Toast.LENGTH_SHORT).show();
 						}
 					}
 				});
 			}
 		}.start();
 	}
+	
 	private ArrayList<BluetoothDevice> availableArduinos = new ArrayList<BluetoothDevice>();
 
 	private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
@@ -329,63 +373,16 @@ public class ControllerActivity extends Activity {
 	private void connect() {
 		if (scanFlag == false) {
 			if (mDevice != null) {
-				Toast.makeText(ControllerActivity.this, "Connected to: " + mDevice.getName(), Toast.LENGTH_SHORT).show();
+				Toast.makeText(BumperBotController.this, "Connected to: " + mDevice.getName(), Toast.LENGTH_SHORT).show();
 				mDeviceAddress = mDevice.getAddress();
 				mBluetoothLeService.connect(mDeviceAddress);
 				scanFlag = true;
 			} else {
-				Toast toast = Toast.makeText(ControllerActivity.this,"Couldn't connect to Arduino. Try again.",Toast.LENGTH_SHORT);
+				Toast toast = Toast.makeText(BumperBotController.this,"Couldn't connect to Arduino. Try again.",Toast.LENGTH_SHORT);
 				toast.show();
 			}
 		}
 	}
-
-	//	private void connect() {
-	//		if (scanFlag == false) {
-	//			scanLeDevice();
-	//
-	//			Timer mTimer = new Timer();
-	//			mTimer.schedule(new TimerTask() {
-	//
-	//				@Override
-	//				public void run() {
-	//					
-	//					
-	//					if (mDevice != null) {
-	//						runOnUiThread(new Runnable() {
-	//							@Override
-	//							public void run() {Toast.makeText(ControllerActivity.this, "Connected to: " + mDevice.getName(), Toast.LENGTH_SHORT).show();}
-	//						});
-	//						mDeviceAddress = mDevice.getAddress();
-	//						mBluetoothLeService.connect(mDeviceAddress);
-	//						scanFlag = true;
-	//					} else {
-	//						runOnUiThread(new Runnable() {
-	//							public void run() {
-	//								Toast toast = Toast
-	//										.makeText(
-	//												ControllerActivity.this,
-	//												"Couldn't connect to Arduino. Try again.",
-	//												Toast.LENGTH_SHORT);
-	//								toast.setGravity(0, 0, Gravity.CENTER);
-	//								toast.show();
-	//							}
-	//						});
-	//					}
-	//				}
-	//			}, SCAN_PERIOD);
-	//		}
-	//
-	//		System.out.println(connState);
-	//		if (connState == false) {
-	//			mBluetoothLeService.connect(mDeviceAddress);
-	//		} else {
-	//			mBluetoothLeService.disconnect();
-	//			mBluetoothLeService.close();
-	//			disableButtons();
-	//		}
-	//	}
-
 	private String bytesToHex(byte[] bytes) {
 		char[] hexChars = new char[bytes.length * 2];
 		int v;
@@ -412,70 +409,81 @@ public class ControllerActivity extends Activity {
 		return newString.toString();
 	}
 
-	private void disableButtons() {
+	@Override
+	protected void onStop() {
+		super.onStop();
+
 		flag = false;
-		connState = false;
 
-		leftButton.setEnabled(false);
-		rightButton.setEnabled(false);
-//		upButton.setEnabled(false);
+		unregisterReceiver(mGattUpdateReceiver);
 	}
 
-	private void enableButtons() {
-		flag = true;
-		connState = true;
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
 
-		leftButton.setEnabled(true);
-		rightButton.setEnabled(true);
-//		upButton.setEnabled(true);
+		if (mServiceConnection != null)
+			unbindService(mServiceConnection);
 	}
 
-	OnTouchListener buttonTouchListener = new OnTouchListener() {
-
-		@Override
-		public boolean onTouch(View v, MotionEvent event) {
-			if(scanFlag) {
-				if(v.equals(leftButton)) {
-					if(event.getAction() == MotionEvent.ACTION_DOWN) {
-						//User just pressed left button
-						//you won't know they lifted up until ACTION_UP is called
-						characteristicTx.setValue(new byte[] {0x01});
-						mBluetoothLeService.writeCharacteristic(characteristicTx);
-					}
-					else if(event.getAction() == MotionEvent.ACTION_UP) {
-						//User just lifted up from button
-						//you probably want to do something like send a "clear" message
-					}
-				}
-				else if(v.equals(upButton)) {
-					if(event.getAction() == MotionEvent.ACTION_DOWN) {
-						//User just pressed up button
-						characteristicTx.setValue(new byte[] {0x02});
-						mBluetoothLeService.writeCharacteristic(characteristicTx);
-					}
-					else if(event.getAction() == MotionEvent.ACTION_UP) {
-						//User just lifted up from button
-						//you probably want to do something like send a "clear" message
-
-					}
-				}
-				else if(v.equals(rightButton)) {
-					if(event.getAction() == MotionEvent.ACTION_DOWN) {
-						//User just pressed right button
-						characteristicTx.setValue(new byte[] {0x03});
-						mBluetoothLeService.writeCharacteristic(characteristicTx);
-					}
-					else if(event.getAction() == MotionEvent.ACTION_UP) {
-						//User just lifted up from button
-						//you probably want to do something like send a "clear" message
-
-					}
-				}
-				
-			}
-			return false;
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// User chose not to enable Bluetooth.
+		if (requestCode == REQUEST_ENABLE_BT
+				&& resultCode == Activity.RESULT_CANCELED) {
+			finish();
+			return;
 		}
-	};
 
-
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	private void forward() {
+		byte[] buf = new byte[] { (byte) 0x03, (byte) 165, (byte) 0x00 };
+		Log.d("Wheel", "left 165");
+		characteristicTx.setValue(buf);
+		mBluetoothLeService.writeCharacteristic(characteristicTx);
+		
+		buf[0] = 0x05;
+		buf[1] = 20;
+		Log.d("Wheel", "right 20");
+		characteristicTx.setValue(buf);
+		mBluetoothLeService.writeCharacteristic(characteristicTx);
+	}
+	
+	private void left() {
+		byte[] buf = new byte[] { (byte) 0x03, (byte) 20, (byte) 0x00 };
+		Log.d("Wheel", "left 20");
+		characteristicTx.setValue(buf);
+		mBluetoothLeService.writeCharacteristic(characteristicTx);
+		
+		buf[0] = 0x05;
+		Log.d("Wheel", "right 20");
+		characteristicTx.setValue(buf);
+		mBluetoothLeService.writeCharacteristic(characteristicTx);
+	}
+	
+	private void right() {
+		byte[] buf = new byte[] { (byte) 0x03, (byte) 165, (byte) 0x00 };
+		Log.d("Wheel", "left 165");
+		characteristicTx.setValue(buf);
+		mBluetoothLeService.writeCharacteristic(characteristicTx);
+		
+		buf[0] = 0x05;
+		Log.d("Wheel", "right 165");
+		characteristicTx.setValue(buf);
+		mBluetoothLeService.writeCharacteristic(characteristicTx);
+	}
+	
+	private void stop() {
+		byte[] buf = new byte[] { (byte) 0x03, (byte) 95, (byte) 0x00 };
+		Log.d("Wheel", "left 0");
+		characteristicTx.setValue(buf);
+		mBluetoothLeService.writeCharacteristic(characteristicTx);
+		
+		buf[0] = 0x05;
+		Log.d("Wheel", "right 0");
+		characteristicTx.setValue(buf);
+		mBluetoothLeService.writeCharacteristic(characteristicTx);
+	}
 }
